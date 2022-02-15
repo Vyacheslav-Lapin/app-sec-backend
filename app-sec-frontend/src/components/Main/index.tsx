6626693bd15f619e8095f12f35959c72cc0463b2
VLapin <Vyacheslav.Lapin@gmail.com>
2022-02-15 13:11:00 +0300

    front-refactor

--- app-sec-frontend/src/components/Main/index.tsx
+++ app-sec-frontend/src/components/Main/index.tsx
@@ -5,20 +5,15 @@ import Home from "../Home";
 
 export default () => <main>
   <BrowserRouter>
-    {/*<Routes>*/}
-    {/*  <Route exact path="/" element={<Home/>}>*/}
-    {/*    <Home/>*/}
-    {/*  </Route>*/}
-    {/*</Routes>*/}
+    <NavLink to="/">Home</NavLink>
+    {/*<NavLink to="/roster">Roster</NavLink>*/}
+    {/*<NavLink to="/schedule">Schedule</NavLink>*/}
+    <NavLink to="/users">Users</NavLink>
     <Routes>
       <Route path='/' element={<Home/>}/>
       {/*<Route path='/roster' element={<Roster/>}/>*/}
       {/*<Route path='/schedule' element={<Schedule/>}/>*/}
       <Route path='/users' element={<UsersView/>}/>
     </Routes>
-    <NavLink to="/">Home</NavLink>
-    {/*<NavLink to="/roster">Roster</NavLink>*/}
-    {/*<NavLink to="/schedule">Schedule</NavLink>*/}
-    <NavLink to="/users">Users</NavLink>
   </BrowserRouter>
 </main>;
